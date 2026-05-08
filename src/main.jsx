1c20291d034cebf13baf14c7786776e6dc9cf623
jeet-khattar <jeetkhattar22@gmail.com>
2026-05-08 13:07:36 +0530

    add clerk temporary again

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 
-// const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <ClerkProvider publishableKey={clerkPubKey}>
-      
-    </ClerkProvider> */}
-    <App />
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <App />
+    </ClerkProvider>
   </StrictMode>
 );
