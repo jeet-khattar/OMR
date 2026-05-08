146d89f3cf483bbcfa16a0f0f13b7f14aa762241
jeet-khattar <jeetkhattar22@gmail.com>
2026-05-08 13:02:55 +0530

    removed clerk temporary

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+// const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={clerkPubKey}>
-      <App />
-    </ClerkProvider>
+    {/* <ClerkProvider publishableKey={clerkPubKey}>
+      
+    </ClerkProvider> */}
+    <App />
   </StrictMode>
 );
